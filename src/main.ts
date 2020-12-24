cc43786ceae72db1cb098d46167855eef140f1ad
teeqq <5570826+teeqq@user.noreply.gitee.com>
2020-12-24 17:31:40 +0800

    人事管理图表

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import './assets/index.ts'
 import router from './router/index'
 import ElementPlus from 'element-plus'
 import '../node_modules/element-plus/lib/theme-chalk/index.css'
+import * as echarts from 'echarts'
+import myEcharts from './utils/echart';
+
 const app = createApp(App)
+app.use(echarts)
+app.config.globalProperties.$echarts = echarts
+app.config.globalProperties.$myEcharts = myEcharts
 app.use(router).use(ElementPlus).mount('#app')
