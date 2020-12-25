c7644ff2275a165e132f3c117d1b165d53896f0c
liangcaxia <836513735@qq.com>
2020-12-25 11:13:53 +0800

    添加函数

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,13 @@ import '../node_modules/element-plus/lib/theme-chalk/index.css'
 import locale from '../node_modules/element-plus/lib/locale/lang/zh-cn'
 import * as echarts from 'echarts'
 import myEcharts from './utils/echart';
+import { formatTimestamp } from './utils/common.ts'//引入日期、格式化内容
+
 
 const app = createApp(App)
 app.use(echarts)
 app.config.globalProperties.$echarts = echarts
 app.config.globalProperties.$myEcharts = myEcharts
+app.config.globalProperties.$formatTimestamp = formatTimestamp
 app.use(router).use(ElementPlus,{ locale }).mount('#app')
+ 
--- src/views/journal/login.vue
+++ src/views/journal/login.vue
@@ -13,7 +13,7 @@
       </div>
       <div class="form-item flex">
         <div class="label">类型</div>
-        <el-select v-model="type" placeholder="请选择"  @change="handleListData">
+        <el-select v-model="type" placeholder="请选择" @change="handleListData">
           <el-option
             v-for="item in options"
             :key="item.value"
@@ -24,12 +24,27 @@
         </el-select>
       </div>
       <div class="form-item ip-input">
-        <el-input placeholder="输入IP地址搜索" suffix-icon="el-icon-search" v-model="keyword"  @keydown.enter.native="handleListData"/>
+        <el-input
+          placeholder="输入IP地址搜索"
+          suffix-icon="el-icon-search"
+          v-model="keyword"
+          @keydown.enter.native="handleListData"
+        />
       </div>
     </div>
     <div class="table-warpper">
-      <el-table ref="multipleTable" header-cell-class-name="table-header" :data="tableData" style="width: 100%" :row-class-name="rowClassName">
-        <el-table-column prop="time" label="时间" />
+      <el-table
+        ref="multipleTable"
+        header-cell-class-name="table-header"
+        :data="tableData"
+        style="width: 100%"
+        :row-class-name="rowClassName"
+      >
+        <el-table-column label="时间">
+          <template  #default="scope">
+            {{ $formatTimestamp(scope.row.time,'second') }}
+          </template>
+        </el-table-column>
         <el-table-column prop="account" label="账号" />
         <el-table-column prop="type" label="类型" />
         <el-table-column prop="ip" label="地址" />
@@ -48,7 +63,7 @@
   </div>
 </template>
 <script lang="ts">
-import {computed,ref} from 'vue'
+import { computed, ref, onMounted } from "vue";
 export default {
   setup() {
     const options = [
@@ -65,25 +80,70 @@ export default {
         label: "登出",
       },
     ];
-    const timeRange = ref([])
-    const  type= ref("全部")
-    const  keyword=ref("")
-    const  tableData=ref([])
-    const  pageNumber= ref(1)
-    const  pageSize= ref(10)
-    const  total= ref(5)
-    const  listParams=computed(() => {
+    const timeRange = ref([]);
+    const type = ref("全部");
+    const keyword = ref("");
+    const tableData = ref([]);
+    const pageNumber = ref(1);
+    const pageSize = ref(10);
+    const total = ref(5);
+    const listParams = computed(() => {
       return {
-        pageNumber:pageNumber.value,
-        timeRange:timeRange.value,
+        pageNumber: pageNumber.value,
+        timeRange: timeRange.value,
         type: type.value,
-        keyword:keyword.value
-      }
-    })
-    const rowClassName=({ rowIndex })=> {
+        keyword: keyword.value,
+      };
+    });
+    const rowClassName = ({ rowIndex }) => {
       // 已取消置灰
-      return rowIndex%2 === 1 ? 'even-row' : 'odd-row' 
-    }
+      return rowIndex % 2 === 1 ? "even-row" : "odd-row";
+    };
+    //获取列表内容
+    const handleListData = async () => {
+      console.log(listParams.value);
+      tableData.value = [
+        {
+          time: "1608865710276",
+          account: "13513245678",
+          type: "登入",
+          ip: "123.123.123.123",
+        },
+        {
+          time: "1608865710276",
+          account: "13513245678",
+          type: "登入",
+          ip: "123.123.123.123",
+        },
+        {
+          time: "1608865710276",
+          account: "13513245678",
+          type: "登入",
+          ip: "123.123.123.123",
+        },
+        {
+          time: "1608865710276",
+          account: "13513245678",
+          type: "登入",
+          ip: "123.123.123.123",
+        },
+        {
+          time: "1608865710276",
+          account: "13513245678",
+          type: "登入",
+          ip: "123.123.123.123",
+        },
+      ];
+    };
+    onMounted(() => {
+      // 首屏加载的时候触发请求
+      handleListData();
+    });
+    const handleCurrentChange = (currentPage) => {
+      // 切换页码
+      pageNumber.value = currentPage;
+      handleListData();
+    };
     return {
       rowClassName,
       options,
@@ -94,69 +154,9 @@ export default {
       pageNumber,
       pageSize,
       total,
-      listParams
+      handleCurrentChange,
+      handleListData,
     };
-  },
-  
-  // data() {
-  //   return {
-  //     timeRange: [],
-  //     type: "全部",
-  //     ip: "",
-  //     tableData: [],
-  //     pageNumber: 1,
-  //     pageSize: 10,
-  //     total: 5,
-  //   };
-  // },
-  created(){
-    this.handleListData()
-  },
-  // computed: {
-  //   listParams(){
-  //       return {
-  //       pageNumber: this.pageNumber,
-  //       timeRange:this.timeRange,
-  //       type: this.type,
-  //       ip:this.ip
-  //     };
-  //   }
-  // },
-  methods: {
-    handleListData() {
-      console.log(this.listParams);
-      this.tableData= [{
-        time:'2020-09-09 15:30:30',
-        account:'13513245678',
-        type:'登入',
-        ip:'123.123.123.123'
-      },{
-        time:'2020-09-09 15:30:30',
-        account:'13513245678',
-        type:'登入',
-        ip:'123.123.123.123'
-      },{
-        time:'2020-09-09 15:30:30',
-        account:'13513245678',
-        type:'登入',
-        ip:'123.123.123.123'
-      },{
-        time:'2020-09-09 15:30:30',
-        account:'13513245678',
-        type:'登入',
-        ip:'123.123.123.123'
-      },{
-        time:'2020-09-09 15:30:30',
-        account:'13513245678',
-        type:'登入',
-        ip:'123.123.123.123'
-      }]
-    },
-    async handleCurrentChange(currentPage) {
-      // 切换页码
-      this.pageNumber = currentPage;
-      this.handleListData();
-    },
   },
 };
 </script>
