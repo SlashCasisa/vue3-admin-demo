c3d5a4ec3131b985dc6ee25e1f0d1c323b6fd6ed
liangcaxia <836513735@qq.com>
2020-12-30 08:52:12 +0800

    解决app.vue找不到模块问题

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import router from './router/index'
 import ElementPlus from 'element-plus'
 import '../node_modules/element-plus/lib/theme-chalk/index.css'
 import locale from '../node_modules/element-plus/lib/locale/lang/zh-cn'
-import * as echarts from 'echarts'
-import myEcharts from './utils/echart'
-import { formatTimestamp } from './utils/common.ts'//引入日期、格式化内容
+// import * as echarts from 'echarts'
+// import myEcharts from './utils/echart'
+import { formatTimestamp } from './utils/common'//引入日期、格式化内容
 
 
 import "./assets/_variable.scss";
 const app = createApp(App)
 
-app.config.globalProperties.$echarts = echarts
-app.config.globalProperties.$myEcharts = myEcharts
+// app.config.globalProperties.$echarts = echarts
+// app.config.globalProperties.$myEcharts = myEcharts
 app.config.globalProperties.$formatTimestamp = formatTimestamp
 app.use(router).use(ElementPlus,{ locale }).mount('#app')
  
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,13 +1,12 @@
-import { RouteLocationNormalizedLoaded, Router } from 'vue-router'
 declare module 'vue'
 declare module "*.vue" { 
-  import { ComponentOptions } from "vue"; 
-  const componentOptions: ComponentOptions;
-  export default componentOptions; 
+  import Vue from "vue"; 
+  export default Vue; 
 }
 
 // Expansion
 declare module 'vue/types/vue' {
+  import { RouteLocationNormalizedLoaded, Router } from 'vue-router'
   interface Vue {
     $router: Router,
     $route: RouteLocationNormalizedLoaded
